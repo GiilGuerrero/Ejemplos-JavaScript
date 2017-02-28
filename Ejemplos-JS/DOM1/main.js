bd2b177adc9cfa66df887314912e678754846b47
Zazil <zaziil.97@gmail.com>
2017-02-28 13:09:18 -0600

    DOM.

---
+++ Ejemplos-JS/DOM1/main.js
@@ -0,0 +1,5 @@
+//document.write('¡Hola JS!')
+//var cajas = document.getElementsByTagName('div')
+var cajas = document.getElementsByClassName('caja')
+cajas[0].textContent ='Hola mundo'
+cajas[1].innerHTML ='<b> Hola JavaScript </b>'
